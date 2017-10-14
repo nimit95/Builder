91f1ed960eee065338b9ad7f3af49518e173ff35
piyush6348 <piyush6348@gmail.com>
2017-10-14 20:46:08 +0530

    not working

---
+++ app/src/main/java/com/uhack/builder/fragment/ContractorsFragment.java
@@ -0,0 +1,30 @@
+package com.uhack.builder.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.uhack.builder.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContractorsFragment extends Fragment {
+
+
+    public ContractorsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contractors, container, false);
+    }
+
+}
